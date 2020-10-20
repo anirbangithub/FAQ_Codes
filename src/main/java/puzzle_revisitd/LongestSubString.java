28e0131ebd56cd269e3dd5d2506c3a636dfc30a9
Anirban <Anirban@localhost>
2020-10-20 21:29:42 +0530

    comment added

--- src/main/java/puzzle_revisitd/LongestSubString.java
+++ src/main/java/puzzle_revisitd/LongestSubString.java
@@ -14,13 +14,17 @@ public class LongestSubString {
 
 	public static int lengthOfLongestSubstring(String s) {
 		int maxLength=0;
+		//iterating one by one
 		for(int i =0;i<s.length();i++) {
 			int j=i;
 			Set<Character> set=new HashSet<Character>();
+			//checking for the longest substring && at the same time 
+			//if there's any duplicate involved by using set
 			while(j<s.length() && !set.contains(s.charAt(j))) {
 				set.add(s.charAt(j));
 				j++;
 			}
+			//checking if this substring is longer than the previous largest
 			if(set.size()>maxLength)
 				maxLength=set.size();
 		}
